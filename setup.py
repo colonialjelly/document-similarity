a5a8bc348fc9440e10a2f3314173164d34246921
Giorgi Kvernadze <colonialjelly@gmail.com>
2021-02-04 22:51:10 -0700

    gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='docsim',
+    version='',
+    packages=['docsim'],
+    url='',
+    license='',
+    author='Giorgi Kvernadze',
+    author_email='giorgi@cs.utah.edu',
+    description=''
+)
